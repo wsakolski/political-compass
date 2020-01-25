883a130c037ec97d968318bd293fe5f971511e00
Władysław Sakolski <wsakolski@gmail.com>
2020-01-25 21:02:54 +0100

    added router on App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,
+Switch,
+Route, 
+Link,
+useRouteMatch,
+useParams} from "react-router-dom"
+
+
+const Test = () => (
+    <h2>Test</h2>
+);
+
+
+
+const Home = () => (
+    <h2>Home</h2>
+);
+
+
+
+const About = () => (
+    <h2>About</h2>
+);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Political Compass</h2>
+    <Router>
+        <header className="App-header">
+        <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+            <Link to='/test'>Test</Link>
+          </li>
+          <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+        </header> 
+        <Switch>
+          <Route path='/test'>
+            <Test/>
+          </Route>
+          <Route path='/about'>
+            <About/>
+          </Route>
+          <Route path='/'>
+          <Home/>
+        </Route>
         
-      </header>
-    </div>
+        </Switch>
+    </Router>
+
   );
 }
 
