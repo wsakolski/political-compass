abbd6e959defb3703e78e2a6eecf07ee8c6e9efe
Sylwia Szymańska <sylwiaszymanska@MacBook-Pro-Sylwia.local>
2020-02-02 18:48:28 +0100

    Add Simple Question form

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import rootReducer from "../reducers/index";
 
-const store = createStore(rootReducer);
+const rootReducer = combineReducers({
+    rootReducer: rootReducer,
+    form: formReducer
+})
 
-export default store;
+export default createStore(rootReducer)
