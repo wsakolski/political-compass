2d4e3a864f471452ec1f3e9f0934ad0fa59eb5c1
Władysław Sakolski <wsakolski@gmail.com>
2020-05-30 20:56:13 +0200

    added redux and reselect

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux"
 import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
-import store from "./store/index"
+import store from "./redux/store"
 
 
 ReactDOM.render(
