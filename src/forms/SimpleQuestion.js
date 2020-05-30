60517d7e7bba4adfc161c2f3d4f7b26d753654a7
Władysław Sakolski <wsakolski@gmail.com>
2020-05-30 20:20:35 +0200

    redux form out

--- src/forms/SimpleQuestion.js
+++ src/forms/SimpleQuestion.js
@@ -1,5 +1,4 @@
-import React from "react"
-import { Field, reduxForm } from "redux-form"
+import React from 'react'
 
 let SimpleQuestion = props => {
   const { handleSubmit } = props
@@ -8,23 +7,16 @@ let SimpleQuestion = props => {
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="firstName">First Name</label>
-        <Field name="firstName" component="input" type="text" />
       </div>
       <div>
         <label htmlFor="lastName">Last Name</label>
-        <Field name="lastName" component="input" type="text" />
       </div>
       <div>
         <label htmlFor="email">Email</label>
-        <Field name="email" component="input" type="email" />
       </div>
       <button type="submit">Submit</button>
     </form>
   )
 }
-
-SimpleQuestion = reduxForm({
-  form: "contact",
-})(SimpleQuestion)
 
 export default SimpleQuestion
