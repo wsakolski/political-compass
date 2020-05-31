d2ad148652d5533b04dd65b4a03e175bc0f35cdc
Władysław Sakolski <wsakolski@gmail.com>
2020-05-31 20:43:44 +0200

    merge fixes

--- src/components/About.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-
-const About = styled.div`
-  font-size: 20px;
-`
-export const About = () => (<About>About</About>);
---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const AboutContainer = styled.div`
+  font-size: 20px;
+`
+export const About = () => (<AboutContainer>About</AboutContainer>);
