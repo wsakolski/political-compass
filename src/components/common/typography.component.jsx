7b22d79e29f033819e342ba556cddfd82141f58b
Władysław Sakolski <wsakolski@gmail.com>
2020-07-14 23:09:09 +0200

    resolving conflicts

---
+++ src/components/common/typography.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+export const HeadingPrimary = styled.h1`
+  font-size: 40px;
+  font-family: 'IM Fell English SC', serif;
+`
+
---
+++ src/components/home.component.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import { HeadingPrimary } from './common/typography.component'
+
+export const Home = () => <HeadingPrimary>Political Compass</HeadingPrimary>
